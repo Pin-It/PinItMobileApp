d8e2d0dcc263a78cdbd4c30e974f126ef37c6677
Noel Lee <kyl116@ic.ac.uk>
2018-06-01 02:43:28 +0100

    Added constructor to Pin model

--- app/src/main/java/com/pinit/api/models/Pin.java
+++ app/src/main/java/com/pinit/api/models/Pin.java
@@ -1,9 +1,31 @@
 package com.pinit.api.models;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Pin {
+  private static final String KEY_TYPE = "pin_type";
+  private static final String KEY_LATITUDE = "latitude";
+  private static final String KEY_LONGITUDE = "longitude";
+
   private Type type;
   private double latitude;
   private double longitude;
+
+  public Pin(JSONObject json) {
+    if (json == null) {
+      throw new IllegalArgumentException("Cannot initialize Pin object from null");
+    }
+
+    try {
+      this.type = Type.fromInt(json.getInt(KEY_TYPE));
+      this.latitude = json.getDouble(KEY_LATITUDE);
+      this.longitude = json.getDouble(KEY_LONGITUDE);
+    } catch (JSONException e) {
+      e.printStackTrace();
+      throw new IllegalArgumentException("Malformed JSON, incompatible with Pin model");
+    }
+  }
 
   public Pin(Type type, double latitude, double longitude) {
     this.type = type;
@@ -17,6 +39,10 @@ public class Pin {
   }
 
   public enum Type {
-    PICKPOCKET, DRUNK, ROBBERY, SCAM, HARASSMENT, OTHERS
+    PICKPOCKET, DRUNK, ROBBERY, SCAM, HARASSMENT, OTHERS;
+
+    public static Type fromInt(int i) {
+      return Type.values()[i - 1];
+    }
   }
 }
