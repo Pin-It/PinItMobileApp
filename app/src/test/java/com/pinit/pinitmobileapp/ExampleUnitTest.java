59cb018be2054c22d381dd35621a8679c65e652b
Noel Lee <kyl116@ic.ac.uk>
2018-06-01 05:03:46 +0100

    Fail a unit test on purpose to test CI

--- app/src/test/java/com/pinit/pinitmobileapp/ExampleUnitTest.java
+++ app/src/test/java/com/pinit/pinitmobileapp/ExampleUnitTest.java
@@ -12,6 +12,7 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+        // Fail on purpose to test CI
+        assertEquals(4, 2 + 1);
     }
 }
