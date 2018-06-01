38a09a2ac4f093d381b09339d2ad6859e89dfb6b
Noel Lee <kyl116@ic.ac.uk>
2018-06-01 05:13:37 +0100

    Reverted failing unit test

--- app/src/test/java/com/pinit/pinitmobileapp/ExampleUnitTest.java
+++ app/src/test/java/com/pinit/pinitmobileapp/ExampleUnitTest.java
@@ -12,7 +12,6 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-        // Fail on purpose to test CI
-        assertEquals(4, 2 + 1);
+        assertEquals(4, 2 + 2);
     }
 }
