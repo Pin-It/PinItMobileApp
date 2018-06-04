ca928160cafbc2742b2f39d6883025fc97e6e1f7
Noel Lee <kyl116@ic.ac.uk>
2018-06-04 01:54:31 +0100

    Added login method in API

--- app/src/main/java/com/pinit/api/PinItAPI.java
+++ app/src/main/java/com/pinit/api/PinItAPI.java
@@ -11,8 +11,58 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PinItAPI {
-    private static final String API_URL = "https://pin-it-app.herokuapp.com/api/";
+    private static final String BASE_URL = "https://pin-it-app.herokuapp.com/";
+    private static final String API_URL = BASE_URL + "api/";
     private static final String PINS_URL = API_URL + Pin.API_ENDPOINT + "/";
+    private static final String TOKEN_AUTH_URL = BASE_URL + "api-token-auth/";
+
+    private static final String TOKEN_FIELD = "token";
+    private static final String USERNAME_FIELD = "username";
+    private static final String PASSWORD_FIELD = "password";
+
+    private String token;
+
+    public PinItAPI() {
+        this(null);
+    }
+
+    public PinItAPI(String token) {
+        this.token = token;
+    }
+
+    public void login(RequestQueue requestQueue, String email, String password, final LoginListener listener) {
+        JSONObject json = new JSONObject();
+        try {
+            json.put(USERNAME_FIELD, email);
+            json.put(PASSWORD_FIELD, password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        NetworkUtils.postJSONObject(requestQueue, TOKEN_AUTH_URL, json, new NetworkListener<JSONObject>() {
+            @Override
+            public void onReceive(JSONObject response) {
+                if (response.has(TOKEN_FIELD)) {
+                    try {
+                        token = response.getString(TOKEN_FIELD);
+                        listener.onSuccess();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        throw new RuntimeException(e);
+                    }
+                } else {
+                    // Wrong email/password combination
+                    listener.onCredentialsError();
+                }
+            }
+
+            @Override
+            public void onError(VolleyError error) {
+                listener.onNetworkError(error);
+            }
+        });
+    }
 
     public static void getAllPins(RequestQueue requestQueue, final NetworkListener<List<Pin>> listener) {
         NetworkUtils.requestJSONArray(requestQueue, PINS_URL, new NetworkListener<JSONArray>() {
@@ -43,5 +93,13 @@ public class PinItAPI {
 
     public static void uploadNewPin(RequestQueue requestQueue, Pin pin, NetworkListener<JSONObject> listener) {
         NetworkUtils.postJSONObject(requestQueue, PINS_URL, pin.toJSONObject(), listener);
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
     }
 }
