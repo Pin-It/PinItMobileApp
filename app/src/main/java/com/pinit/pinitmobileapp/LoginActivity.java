c890287f8f921f851a474fee6b7cbbb3cbe83971
Noel Lee <leekayau@gmail.com>
2018-06-06 23:21:32 +0100

    Cleaned up unnecessary code

--- app/src/main/java/com/pinit/pinitmobileapp/LoginActivity.java
+++ app/src/main/java/com/pinit/pinitmobileapp/LoginActivity.java
@@ -47,13 +47,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      */
     private static final int REQUEST_READ_CONTACTS = 0;
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
     private static final String PREFERENCE_USER_TOKEN = "userToken";
 
     /**
@@ -67,7 +60,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private View mProgressView;
     private View mLoginFormView;
     private Intent intent;
-    private TextView textView;
     private Button mEmailSignInButton;
 
 
@@ -78,9 +70,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         setContentView(R.layout.activity_login);
 
         checkLoginStatus();
-
-        textView = (TextView)findViewById(R.id.simpleTextView);
-
 
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
