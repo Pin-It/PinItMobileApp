5e307fb1186d8baf4e3236bb609c9d16db4fb81d
Yoon Kim <jyk416@ic.ac.uk>
2018-06-07 07:36:58 +0100

    Open the explanation activity only when it's first installed

--- app/src/main/java/com/pinit/pinitmobileapp/ExplanationActivity.java
+++ app/src/main/java/com/pinit/pinitmobileapp/ExplanationActivity.java
@@ -1,6 +1,7 @@
 package com.pinit.pinitmobileapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,10 +15,15 @@ public class ExplanationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_explanation);
-
         final String token = getIntent().getExtras().getString(MapsActivity.USER_TOKEN, null);
 
+        if (!isFirstTime()) {
+            Intent nextIntent = new Intent(ExplanationActivity.this, MapsActivity.class);
+            nextIntent.putExtra(MapsActivity.USER_TOKEN, token);
+            startActivity(nextIntent);
+            finish();
+        }
+        setContentView(R.layout.activity_explanation);
 
         fabuno = findViewById(R.id.fab1);
         fabdos = findViewById(R.id.fab2);
@@ -41,5 +47,16 @@ public class ExplanationActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private boolean isFirstTime() {
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        boolean ranBefore = preferences.getBoolean("RanBefore", false);
+        if (!ranBefore) {
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean("RanBefore",true);
+            editor.commit();
+        }
+        return !ranBefore;
     }
 }
--- app/src/main/java/com/pinit/pinitmobileapp/LoginActivity.java
+++ app/src/main/java/com/pinit/pinitmobileapp/LoginActivity.java
@@ -153,17 +153,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 .apply();
     }
 
-    private boolean isFirstTime() {
-        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
-        boolean ranBefore = preferences.getBoolean("RanBefore", false);
-        if (!ranBefore) {
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putBoolean("RanBefore",true);
-            editor.commit();
-        }
-        return !ranBefore;
-    }
-
     private void populateAutoComplete() {
         if (!mayRequestContacts()) {
             return;
